f4d384ac492f57eab7da92d20e34dee7daa87260
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2020-01-05 22:51:37 +0100

    form profil

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
+import { decodeToken } from '../../helpers/token';
+import { User } from '../../interfaces/user.interfaces';
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  public userForm = this.formBuilder.group({
+    nickname: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private router: Router, private formBuilder: FormBuilder, private userService: UsersService) {
   }
 
+  ngOnInit() {
+    this.userService.getInfos({ uuid: decodeToken().uuid }).subscribe((res: User) => {
+      return this.userForm.setValue({ nickname: res.nickname, email: res.email });
+    }, error => {
+      console.log(error);
+      return error;
+    });
+  }
+
+  submitFrom(e: Event) {
+    e.preventDefault();
+    // return this.userService
+    //   .registerUser({
+    //     nickname: this.userForm.value.nickname,
+    //     password: this.userForm.value.password,
+    //     passwordConfirm: this.userForm.value.passwordConfirm,
+    //     email: this.userForm.value.email
+    //   })
+    //   .subscribe(res => {
+    //     // console.log(res);
+    //     // localStorage.setItem('token', res.meta.token);
+    //     this.router.navigate(['/', 'login']);
+    //     return res;
+    //   }, error => {
+    //     console.log(error);
+    //     this.hidden = false;
+    //     this.error = error;
+    //     setTimeout(() => {
+    //       this.error = undefined;
+    //     }, 2000);
+    //     return error;
+    //   }, () => {
+    //     this.hidden = false;
+    //   });
+  }
 }
--- src/app/interfaces/user.interfaces.ts
+++ src/app/interfaces/user.interfaces.ts
@@ -1,7 +1,7 @@
 export interface User {
   uuid?: string;
   nickname?: string;
-  email: string;
+  email?: string;
   password?: string;
   passwordConfirm?: string;
   createAt?: string;
